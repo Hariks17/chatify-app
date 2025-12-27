59de13f3406ae6634c503e410c68fee739c22a35
Hariks17 <hariksh.94@gmail.com>
2025-12-27 15:32:48 +0530

    Added Middleware

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import path from "path";
+import cookieParser from "cookie-parser";
 import authRoute from "./routes/auth.route.js";
 import messageRoute from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
@@ -13,6 +14,7 @@ const __dirname = path.resolve();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoute);
 app.use("/api/messages", messageRoute);
